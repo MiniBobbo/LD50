d6e0f23b240c8514c23fd5742607f18b5bf82117
Dave M <you@example.com>
2022-04-08 13:43:48 -0400

    Further movement work

--- src/FSM/NinjaFSM.ts
+++ src/FSM/NinjaFSM.ts
@@ -43,7 +43,7 @@ export class NinjaFSM extends FSMModule {
     }
 
     TryJump() {
-        //Check the screen position and not the world position.  That will mess up depending on the camera.
+        //Calculate the cursor world position.  That will mess up depending on the camera.
         let cam = this.gs.cameras.main;
         let cpos = {x: cam.scrollX + this.gs.cursor.x, y: cam.scrollY + this.gs.cursor.y}; 
         let spos = {x: this.p.sprite.x, y: this.p.sprite.y}; 
@@ -64,9 +64,29 @@ export class NinjaFSM extends FSMModule {
                     this.Jump(cpos);
                 }
                 else {
-                    // this.target.x = this.gs.cursor.x;
-                    // this.target.y = this.p.sprite.y;
-                    // this.Run();
+                    if(cpos < spos) {
+                        //Check for left flipup
+                        if(!C.GetTile(spos.x, spos.y - C.NINJA_FLIP_DISTANCE).collides) {
+                            this.p.sprite.x += C.TILE_SIZE;
+                            this.Flip(D.UL);
+                        }
+                        else if(cpos.x < spos.x) {
+                            if(!C.GetTile(spos.x - C.NINJA_FLIP_DISTANCE, spos.y - C.NINJA_FLIP_DISTANCE).collides)
+                                this.Flip(D.UL);
+                        } 
+
+                    } else {
+                        //Check for right flipup
+                        if(!C.GetTile(spos.x, spos.y - C.NINJA_FLIP_DISTANCE).collides) {
+                            this.p.sprite.x -= C.TILE_SIZE;
+                            this.Flip(D.UR);
+                        }
+                        else if(cpos.x < spos.x) {
+                            if(!C.GetTile(spos.x + C.NINJA_FLIP_DISTANCE, spos.y - C.NINJA_FLIP_DISTANCE).collides)
+                                this.Flip(D.UR);
+                        } 
+
+                    }
                 }
             
                 break;
@@ -179,11 +199,13 @@ export class NinjaFSM extends FSMModule {
         }  else if(this.running) {
             if(!this.p.sprite.body.blocked.down) {
                 //Insetad of just falling, swing down and grab into the ledge that we just fell from.
-                this.FlipDown();
+                this.Flip(D.D);
             } else if(this.p.sprite.body.velocity.x > 0 && this.p.sprite.x >= this.target.x) {
                 this.StopRun();
+                this.p.PlayAnimation('crouch');
             } else if(this.p.sprite.body.velocity.x < 0 && this.p.sprite.x <= this.target.x) {
                 this.StopRun();
+                this.p.PlayAnimation('crouch');
             } else if(this.p.sprite.body.blocked.left) {
                 this.dir = D.L;
                 this.p.PlayAnimation('wallgrab');
@@ -198,22 +220,38 @@ export class NinjaFSM extends FSMModule {
     }
 
     
-    FlipDown() {
-        this.p.sprite.y += 10;
+    Flip(direction:D) {
+        //TODO:  Add a delay fo the animation here.
+
         this.p.sprite.setVelocity(0,0);
         this.p.sprite.setGravity(0,0);
         this.StopRun();
-        this.p.PlayAnimation('wallgrab');
-        //TODO: Add animation here.  
-        if(this.p.sprite.flipX) {
+
+        if(direction == D.D) {
+            this.p.sprite.y += 10;
+            this.p.PlayAnimation('wallgrab');
+            //TODO: Add animation here.  
+            if(this.p.sprite.flipX) {
+                this.dir = D.R;
+            } else
+            this.dir = D.L;
+        } else if (direction == D.UL) {
+            let pos = C.RoundToTile(this.p.sprite.x - C.TILE_SIZE, this.p.sprite.y - C.TILE_SIZE);
+            this.p.sprite.setPosition(pos.x+4, pos.y);
             this.dir = D.R;
-        } else
-        this.dir = D.L;
+            this.p.PlayAnimation('wallgrab');
+            this.p.sprite.flipX = true;
+        } else if (direction == D.UR) {
+            let pos = C.RoundToTile(this.p.sprite.x + C.TILE_SIZE, this.p.sprite.y - C.TILE_SIZE);
+            this.p.sprite.setPosition(pos.x-4, pos.y);
+            this.dir = D.L;
+            this.p.PlayAnimation('wallgrab');
+            this.p.sprite.flipX = false;
+        }
 
 }
     StopRun() {
         this.p.sprite.setVelocityX(0);
         this.running = false;
-        this.p.PlayAnimation('crouch');
     }
 }
